d96fd9cfeaefbe05379c79e03c4d0c2fc91019a9
Lopeznievaalberto <lopeznievaalberto@gmail.com>
2023-01-14 19:33:35 +0100

    actualizacion completa de base de datos1~

--- router.js
+++ router.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 
 const moviesRouter = require('./views/moviesRouter');
 const seriesRouter = require('./views/seriesRouter');
-const usersRouter = require('./views/usersRouter');
 
 router.use("/movies", moviesRouter);
 router.use("/series", seriesRouter);
